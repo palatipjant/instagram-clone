14138f29a9e9ce83065ce9c4361a10d40b9c8929
Palatip Jantawong <palatipjant@outlook.com>
2023-10-16 12:54:23 +0700

    Initial commit

---
+++ instagram-clone/instagram_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_cloneApp.swift
+//  instagram-clone
+//
+//  Created by Palatip Jantawong on 16/10/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct instagram_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
